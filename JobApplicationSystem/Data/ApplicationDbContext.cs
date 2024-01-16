1ce27f69d11957b1a3e4f3678ead682aa8e6debb
Dijane Hoti <dhotii002@gmail.com>
2024-01-16 13:28:13 +0100

    Database added

---
+++ JobApplicationSystem/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobApplicationSystem.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options)
+          : base(options)
+        {
+        }
+    }
+}
